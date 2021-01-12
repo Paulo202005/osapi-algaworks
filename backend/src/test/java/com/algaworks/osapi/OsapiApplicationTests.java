a8ae505d0a072f003475056bca960bfe971237cd
Paulo202005 <paulo.argenton@gmail.com>
2021-01-12 11:00:07 -0300

    Project created

---
+++ backend/src/test/java/com/algaworks/osapi/OsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.osapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
